23d51365df88100a66cdbe4fe8f8f844a1c0a9e9
Johnjohnn <conde2010@aol.com>
2020-07-19 14:02:18 -0400

    finished

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from "./data"
+import Card from "./components/card"
+import Form from "./components/form"
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [members, setMembers] = useState(data);
+  const addNewMembers = member => {
+    setMembers ([...members, member]);
+  };
+
+return (
+  <div className="App">
+    <Form addNewMembers = {addNewMembers}/>
+    <Card member={members} />
+   
+  </div>
+);
 }
 
 export default App;
---
+++ team-builder/src/data.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        id: 1,
+        name: "Jonathan M",
+        email: "conde2010gmail.com",
+        role: "Front End Developer"
+    },
+    {
+        id: 2,
+        name: "frank osorio",
+        email: "frank_osoriog@gmail.com",
+        role: "Back End Developer"
+    }
+]
